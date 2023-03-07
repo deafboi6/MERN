aaa894e747d00ddc370bb4bacb24a29a3ee71124
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-07 09:41:39 -0800

    removed typo from authLink ftn

--- client/src/App.js
+++ client/src/App.js
@@ -16,7 +16,7 @@ const httpLink = createHttpLink({
   uri: '/graphql',
 });
 
-const authLink = setContext((_, { headers }) => {s
+const authLink = setContext((_, { headers }) => {
   const token = localStorage.getItem('id_token');
   return {
     headers: {
