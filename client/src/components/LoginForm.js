24a05c87232324464204472553e4fb6084d30679
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-07 09:49:28 -0800

    changes from API login to mutation login

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -1,9 +1,7 @@
-// see SignupForm.js for comments
 import React, { useState } from 'react';
 import { Form, Button, Alert } from 'react-bootstrap';
-
-import { loginUser } from '../utils/API';
 import Auth from '../utils/auth';
+
 //------ NEW ADDITIONS --------//
 import { LOGIN_USER } from './utils/mutations';
 import { useMutation } from '@apollo/client';
@@ -12,6 +10,7 @@ const LoginForm = () => {
   const [userFormData, setUserFormData] = useState({ email: '', password: '' });
   const [validated] = useState(false);
   const [showAlert, setShowAlert] = useState(false);
+  const [login, { error, data }] = useMutation(LOGIN_USER);
 
   const handleInputChange = (event) => {
     const { name, value } = event.target;
@@ -29,7 +28,7 @@ const LoginForm = () => {
     }
 
     try {
-      const response = await loginUser(userFormData);
+      const { data } = await login({ variables: { ...userFormData } });
 
       if (!response.ok) {
         throw new Error('something went wrong!');
