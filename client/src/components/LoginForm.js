afa1ba2ae324c61c9ff06b0625210b64866fc629
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-07 09:50:00 -0800

    removes if statement from login process

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -30,10 +30,6 @@ const LoginForm = () => {
     try {
       const { data } = await login({ variables: { ...userFormData } });
 
-      if (!response.ok) {
-        throw new Error('something went wrong!');
-      }
-
       const { token, user } = await response.json();
       console.log(user);
       Auth.login(token);
