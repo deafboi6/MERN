e2dd8dc4159f7ffbf20206b798b37faa5d052140
Jayden Chapman <jaychapmancode@gmail.com>
2023-03-07 09:52:32 -0800

    corrected import for LOGIN_USER

--- client/src/components/LoginForm.js
+++ client/src/components/LoginForm.js
@@ -3,7 +3,7 @@ import { Form, Button, Alert } from 'react-bootstrap';
 import Auth from '../utils/auth';
 
 //------ NEW ADDITIONS --------//
-import { LOGIN_USER } from './utils/mutations';
+import { LOGIN_USER } from '../utils/mutations';
 import { useMutation } from '@apollo/client';
 
 const LoginForm = () => {
